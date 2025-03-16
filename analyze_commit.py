fa99b3af2cf7e6de44afbf04071f1a8069431ca4
DishaAdiga <dishasaligrama1@gmail.com>
2025-03-16 16:57:49 +0530

    Analyze commit using text blob

---
+++ analyze_commit.py
@@ -0,0 +1,14 @@
+import sys
+from textblob import TextBlob
+
+def analyze_commit(commit_msg):
+    blob = TextBlob(commit_msg)
+    
+    # Example: Block commits with negative sentiment or short messages
+    if len(commit_msg) < 10 or blob.sentiment.polarity < 0:
+        return "BLOCK: Commit message is too short or negative"
+    return "PASS: Commit message is good"
+
+if __name__ == "__main__":
+    commit_message = sys.argv[1]
+    print(analyze_commit(commit_message))
