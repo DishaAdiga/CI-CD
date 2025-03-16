c5a8fd3621b41e0807ff068b719d32aada5802f4
DishaAdiga <dishasaligrama1@gmail.com>
2025-03-16 17:34:00 +0530

    Modified threshold value for rejection of commit messages

--- analyze_commit.py
+++ analyze_commit.py
@@ -5,7 +5,7 @@ def analyze_commit(commit_msg):
     blob = TextBlob(commit_msg)
     
     # Example: Block commits with negative sentiment or short messages
-    if len(commit_msg) < 10 or blob.sentiment.polarity < 0:
+    if len(commit_msg) < 10 or blob.sentiment.polarity < -0.25:
         return "BLOCK: Commit message is too short or negative"
         sys.exit(1)
     return "PASS: Commit message is good"
